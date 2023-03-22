30f92c2fa988e9dc734dfe949c1b9e0768c341a3
Bahar <38427417+BaharAdineh@users.noreply.github.com>
2023-03-22 23:04:27 +0100

    Trying to improve SecurityConfig

--- src/main/java/com/challenge/ivms/config/AppConfig.java
+++ src/main/java/com/challenge/ivms/config/AppConfig.java
@@ -1,42 +1,39 @@
 package com.challenge.ivms.config;
 
-
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.sql.DataSource;
-import java.util.Properties;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Configuration
-@EnableWebMvc
-public class AppConfig implements WebMvcConfigurer {
+@EnableMongoRepositories(basePackages = "com.challenge.ivms.repository")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppConfig extends AbstractMongoClientConfiguration {
 
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/inventory");
-        dataSource.setUsername("root");
-        dataSource.setPassword("password");
-        return dataSource;
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoUri;
+
+    @Override
+    public MongoClient mongoClient() {
+        return MongoClients.create(mongoUri);
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        return "mydatabase";
     }
 
     @Bean
-    public LocalSessionFactoryBean sessionFactory() {
-        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-        sessionFactory.setDataSource(dataSource());
-        sessionFactory.setPackagesToScan("com.example.inventory.model");
-        Properties hibernateProperties = new Properties();
-        hibernateProperties.put("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
-        hibernateProperties.put("hibernate.show_sql", true);
-        hibernateProperties.put("hibernate.format_sql", true);
-        sessionFactory.setHibernateProperties(hibernateProperties);
-        return sessionFactory;
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoClient(), getDatabaseName());
     }
 }
-
-
