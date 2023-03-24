93f01c700fa767994f51aa7890b07188481af8ea
Bahar <38427417+BaharAdineh@users.noreply.github.com>
2023-03-24 23:58:06 +0100

    Remove jpa

--- src/main/java/com/challenge/ivms/repository/InventoryRepository.java
+++ src/main/java/com/challenge/ivms/repository/InventoryRepository.java
@@ -1,14 +1,14 @@
 package com.challenge.ivms.repository;
 
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import com.challenge.ivms.model.Inventory;
 
 import java.util.Optional;
 
 @Repository
-public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+public interface InventoryRepository extends MongoRepository<Inventory, Long> {
 
     Optional<Inventory> findByItemId(Long itemId);
 
