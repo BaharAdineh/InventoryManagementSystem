df2ed4ff471974551441aa196c51b34d6ece20ea
Bahar <38427417+BaharAdineh@users.noreply.github.com>
2023-04-09 13:02:08 +0200

    Add IVMS config server

---
+++ ivms-config-server/src/test/java/com/ivms/ivmsconfigserver/IvmsConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivms.ivmsconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IvmsConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
