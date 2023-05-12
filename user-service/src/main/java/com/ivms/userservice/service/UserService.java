95aa006c670abed69270e78b94dd4d127458dcee
Bahar <38427417+BaharAdineh@users.noreply.github.com>
2023-05-12 15:09:26 +0200

    Trying to fix security config

--- user-service/src/main/java/com/ivms/userservice/service/UserService.java
+++ user-service/src/main/java/com/ivms/userservice/service/UserService.java
@@ -1,7 +1,55 @@
 package com.ivms.userservice.service;
 
-public class UserService {
+import com.ivms.userservice.model.User;
 
-    public boolean getUserByUsername(String username) {
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserService {
+    private List<User> users;
+
+    public UserService() {
+        this.users = new ArrayList<>();
+    }
+
+    public User getUserByUsername(String username) {
+        for (User user : users) {
+            if (user.getUsername().equals(username)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    public void removeUser(User user) {
+        users.remove(user);
+    }
+
+    // Other methods for user management (e.g., updateUser, getAllUsers, etc.)
+
+    // ...
+
+    // Inner User class
+    public static class User {
+        private String username;
+        private String password;
+
+        public User(String username, String password) {
+            this.username = username;
+            this.password = password;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
     }
 }
+
