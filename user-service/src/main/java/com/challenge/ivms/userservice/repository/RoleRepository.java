c51898774e337052fd18a81725c17497b0f768b3
Bahar <38427417+BaharAdineh@users.noreply.github.com>
2023-07-03 21:52:33 +0200

    Trying Spring Boot Security JWT

---
+++ user-service/src/main/java/com/challenge/ivms/userservice/repository/RoleRepository.java
@@ -0,0 +1,15 @@
+package com.challenge.ivms.userservice.repository;
+
+
+import java.util.Optional;
+
+import com.challenge.ivms.userservice.model.ERole;
+import com.challenge.ivms.userservice.model.Role;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface RoleRepository extends MongoRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
+}
--- user-service/src/main/java/com/challenge/ivms/userservice/repository/UserRepository.java
+++ user-service/src/main/java/com/challenge/ivms/userservice/repository/UserRepository.java
@@ -1,14 +1,18 @@
 package com.challenge.ivms.userservice.repository;
 
+import java.util.Optional;
 
 import com.challenge.ivms.userservice.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+
+
 @Repository
 public interface UserRepository extends MongoRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 
-    User findByUsername(String username);
+    Boolean existsByUsername(String username);
 
+    Boolean existsByEmail(String email);
 }
-
