c1006119f36b0fdb6e3cfc7ec7a981f77b9ffaa4
Bahar <38427417+BaharAdineh@users.noreply.github.com>
2023-07-26 22:48:11 +0200

    Trying Spring Boot Security JWT

--- user-service/src/main/java/com/challenge/ivms/userservice/model/Role.java
+++ user-service/src/main/java/com/challenge/ivms/userservice/model/Role.java
@@ -2,10 +2,12 @@ package com.challenge.ivms.userservice.model;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@AllArgsConstructor
 @Document(collection = "roles")
 public class Role {
     @Id
