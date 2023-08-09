3ebe09cef1c184ea5434d7e184899d3bfcc9596b
Bahar <38427417+BaharAdineh@users.noreply.github.com>
2023-08-09 22:08:19 +0200

    Improving com.challenge.ivms.orderservice

--- product-service/src/main/java/com/challenge/ivms/productservice/ProductService.java
+++ product-service/src/main/java/com/challenge/ivms/productservice/ProductService.java
@@ -1,10 +1,7 @@
 package com.challenge.ivms.productservice;
 
-import com.challenge.ivms.productservice.model.Product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.List;
 
 @SpringBootApplication
 public class ProductService {
