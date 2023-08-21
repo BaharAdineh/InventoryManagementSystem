3c3b42f1e871114403050389c96c7d55c0e6efd3
Bahar <38427417+BaharAdineh@users.noreply.github.com>
2023-08-21 22:12:10 +0200

    Improving com.challenge.ivms.orderservice

--- order-service/src/main/java/com/challenge/ivms/orderservice/OrderService.java
+++ order-service/src/main/java/com/challenge/ivms/orderservice/OrderService.java
@@ -2,8 +2,10 @@ package com.challenge.ivms.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.challenge.ivms")
 public class OrderService {
 
     public static void main(String[] args) {
